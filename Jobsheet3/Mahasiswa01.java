720d7d0eb919ee06603aeaf8273dd8cdbaab7b73
Abimmmm <156158397+Abimmmm@users.noreply.github.com>
2025-02-28 16:03:14 +0700

    Jobsheet 3

--- Jobsheet3/Mahasiswa01.java
+++ Jobsheet3/Mahasiswa01.java
@@ -4,4 +4,13 @@ public class Mahasiswa01 {
     public String nama;
     public String kelas;
     public float ipk;
+
+    public void cetakInfo(){
+        System.out.println("Data Mahasiswa ke-" );
+        System.out.println("NIM   : " + nim);
+        System.out.println("Nama  : " + nama);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("IPK   : " + ipk);
+        System.out.println("---------------------------------");
+    }
 }
